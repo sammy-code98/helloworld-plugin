b574411a70e13b9ec63dde6b718f94eaccba4a05
sammy-code98 <sammysmart98@yahoo.com>
2021-12-20 20:20:27 +0100

    feat: create entry point and install method

---
+++ src/install.js
@@ -0,0 +1,15 @@
+import HelloWorld from "./components/HelloWorld.vue";
+const HelloWorldSimple = {
+  // Let's register our component globally
+
+  install(Vue, options) {
+    Vue.component("HelloWorld", HelloWorld);
+  },
+};
+
+// Automatic installation if Vue has been added to the global scope.
+if (typeof window !== "undefined" && window.Vue) {
+  window.Vue.use(HelloWorldSimple);
+}
+
+export default HelloWorldSimple;
